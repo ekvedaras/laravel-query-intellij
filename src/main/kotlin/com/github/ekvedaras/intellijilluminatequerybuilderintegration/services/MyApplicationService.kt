c77123626573e9ca5870d5e41c4f03a5de9cbca8
GitHub Action <action@github.com>
2020-12-31 09:51:00 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ekvedaras/intellijilluminatequerybuilderintegration/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ekvedaras.intellijilluminatequerybuilderintegration.services
+
+import com.github.ekvedaras.intellijilluminatequerybuilderintegration.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
