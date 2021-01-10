e4d888eef4d772c8556b1837d0da68948d46b7c2
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2021-01-10 14:29:14 +0100

    Support completing schema names as well when completing table names.
    If schema name was inserted trigger completion again with table only from that schema

--- src/main/kotlin/dev/ekvedaras/intellijilluminatequerybuilderintegration/completion/TableOrViewCompletionProvider.kt
+++ src/main/kotlin/dev/ekvedaras/intellijilluminatequerybuilderintegration/completion/TableOrViewCompletionProvider.kt
@@ -1,22 +1,19 @@
 package dev.ekvedaras.intellijilluminatequerybuilderintegration.completion
 
+import com.intellij.codeInsight.AutoPopupControllerImpl
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.database.util.DasUtil
-import com.intellij.database.util.DbUtil
-import com.intellij.util.ProcessingContext
-
-import com.intellij.database.psi.DbNamespaceImpl
-
 import com.intellij.database.model.DasTable
 import com.intellij.database.symbols.DasPsiWrappingSymbol
+import com.intellij.database.util.DasUtil
+import com.intellij.database.util.DbUtil
 import com.intellij.openapi.project.Project
+import com.intellij.util.ProcessingContext
 import com.jetbrains.php.lang.psi.elements.MethodReference
 import dev.ekvedaras.intellijilluminatequerybuilderintegration.utils.LaravelUtils
 import dev.ekvedaras.intellijilluminatequerybuilderintegration.utils.MethodUtils
-import icons.DatabaseIcons
 
 class TableOrViewCompletionProvider : CompletionProvider<CompletionParameters>() {
     override fun addCompletions(
@@ -34,10 +31,30 @@ class TableOrViewCompletionProvider : CompletionProvider<CompletionParameters>()
             return
         }
 
+        var schema: String? = null
+        if (parameters.position.text.contains(".")) {
+            schema = parameters.position.text.substringBefore(".")
+        }
+
         DbUtil.getDataSources(method.project).forEach { dataSource ->
+            if (schema == null) {
+                DasUtil.getSchemas(dataSource).forEach {
+                    result.addElement(
+                        LookupElementBuilder
+                            .create(it, it.name + ".")
+                            .withIcon(DasPsiWrappingSymbol(it, method.project).getIcon(false))
+                            .withInsertHandler { _, _ ->
+                                AutoPopupControllerImpl.getInstance(method.project).scheduleAutoPopup(parameters.editor)
+                            }
+                    )
+                }
+            }
+
             DasUtil.getTables(dataSource.dataSource)
-                .filter { !it.isSystem }
-                .forEach { result.addElement(buildLookup(it, method.project)) }
+                .filter {
+                    !it.isSystem && (schema == null || it.dasParent?.name == schema)
+                }
+                .forEach { result.addElement(buildLookup(it, schema != null, method.project)) }
         }
     }
 
@@ -45,17 +62,21 @@ class TableOrViewCompletionProvider : CompletionProvider<CompletionParameters>()
         !LaravelUtils.BuilderTableMethods.contains(method.name)
                 || MethodUtils.findParameterIndex(parameters.position) != 0
 
-    private fun buildLookup(table: DasTable, project: Project): LookupElementBuilder {
-        var builder = LookupElementBuilder
-            .create(table, table.name)
+    private fun buildLookup(table: DasTable, prependSchema: Boolean, project: Project): LookupElementBuilder {
+        val tableSchema = table.dasParent ?: return LookupElementBuilder
+            .create(table.name)
             .withIcon(DasPsiWrappingSymbol(table, project).getIcon(false))
 
-        val tableSchema = table.dasParent ?: return builder
-
-        if (tableSchema is DbNamespaceImpl) {
-            builder = builder.withTypeText(tableSchema.parent?.name, true)
+        if (prependSchema) {
+            return LookupElementBuilder
+                .create(table, tableSchema.name + "." + table.name)
+                .withIcon(DasPsiWrappingSymbol(table, project).getIcon(false))
         }
 
-        return builder.withTailText(" (" + tableSchema.name + ")", true)
+        return LookupElementBuilder
+            .create(table.name)
+            .withIcon(DasPsiWrappingSymbol(table, project).getIcon(false))
+            .withTailText(" (" + tableSchema.name + ")", true)
+            .withLookupString(tableSchema.name + "." + table.name)
     }
 }
