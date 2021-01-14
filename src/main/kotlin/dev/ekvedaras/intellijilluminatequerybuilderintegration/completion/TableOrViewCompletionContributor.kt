6fb131f297ac6ae39b4049c9bd5a839f566bf9da
Ernestas Kvedaras <ernestaskvedaras@i3d.net>
2021-01-14 19:01:56 +0100

    Trigger completion after opening the string

--- src/main/kotlin/dev/ekvedaras/intellijilluminatequerybuilderintegration/completion/TableOrViewCompletionContributor.kt
+++ src/main/kotlin/dev/ekvedaras/intellijilluminatequerybuilderintegration/completion/TableOrViewCompletionContributor.kt
@@ -3,9 +3,14 @@ package dev.ekvedaras.intellijilluminatequerybuilderintegration.completion
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiElement
 import com.jetbrains.php.lang.lexer.PhpTokenTypes
 
 class TableOrViewCompletionContributor : CompletionContributor() {
+    override fun invokeAutoPopup(position: PsiElement, typeChar: Char): Boolean {
+        return typeChar == '\'' || typeChar == '"' || super.invokeAutoPopup(position, typeChar)
+    }
+
     init {
         extend(
             CompletionType.BASIC,
