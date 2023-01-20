ba4d4608573e9b22c48ed44a62ff36d788d8be4b
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2023-01-20 21:54:36 +0100

    Readable expectations for database elements completion in tests

--- src/test/kotlin/dev/ekvedaras/laravelquery/integration/tests/completion/TestsCompletionTest.kt
+++ src/test/kotlin/dev/ekvedaras/laravelquery/integration/tests/completion/TestsCompletionTest.kt
@@ -1,35 +1,41 @@
 package dev.ekvedaras.laravelquery.integration.tests.completion
 
 import dev.ekvedaras.laravelquery.BaseTestCase
+import dev.ekvedaras.laravelquery.support.Columns
+import dev.ekvedaras.laravelquery.support.Namespaces
+import dev.ekvedaras.laravelquery.support.Tables
 
 internal class TestsCompletionTest : BaseTestCase() {
-    fun testItCompletesInAssertDatabaseHasCallTableProperty()
-    {
+    fun testItCompletesInAssertDatabaseHasCallTableProperty() {
         myFixture.configureByFile("integration/tests/completion/inAssertDatabaseHasCallTableProperty.php")
         myFixture.completeBasic()
 
-        assertCompletion("testProject1", "testProject2")
-        assertCompletion("users", "customers", "failed_jobs", "migrations")
-        assertNoCompletion("id", "email", "billable_id")
+        Namespaces.expect(myFixture).toBeCompleted()
+        Tables.expect(myFixture).toBeCompleted()
+        Columns.expect(myFixture).not().toBeCompleted()
     }
 
     fun testItCompletesInAssertDatabaseHasCallColumnsPropertyArrayHashKey() {
         myFixture.configureByFile("integration/tests/completion/inAssertDatabaseHasCallColumnsProperty.php")
         myFixture.completeBasic()
 
-        assertCompletion("id", "email")
-        assertNoCompletion("billable_id")
-        assertNoCompletion("testProject1", "testProject2")
-        assertNoCompletion("users", "customers", "failed_jobs", "migrations")
+        Namespaces.expect(myFixture).not().toBeCompleted()
+
+        Tables.users
+            .expect(myFixture)
+            .not().toBeCompleted().asWellAsOthers().andTheirColumns()
+            .but().toHaveItsColumnsCompleted()
     }
 
     fun testItCompletesInAssertDatabaseHasCallColumnsPropertyArrayEntry() {
         myFixture.configureByFile("integration/tests/completion/inAssertDatabaseHasCallColumnsPropertyArrayEntry.php")
         myFixture.completeBasic()
 
-        assertCompletion("id", "email")
-        assertNoCompletion("billable_id")
-        assertNoCompletion("testProject1", "testProject2")
-        assertNoCompletion("users", "customers", "failed_jobs", "migrations")
+        Namespaces.expect(myFixture).not().toBeCompleted()
+
+        Tables.users
+            .expect(myFixture)
+            .not().toBeCompleted().asWellAsOthers().andTheirColumns()
+            .but().toHaveItsColumnsCompleted()
     }
 }
