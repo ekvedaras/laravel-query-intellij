d89df8a4ac995a7d0a05c75d2ebfbd5d0065ae55
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2023-02-26 18:00:20 +0100

    Use whenSmart

--- src/main/kotlin/dev/ekvedaras/laravelquery/domain/query/builder/methods/parameters/QueryParameter.kt
+++ src/main/kotlin/dev/ekvedaras/laravelquery/domain/query/builder/methods/parameters/QueryParameter.kt
@@ -1,17 +1,13 @@
 package dev.ekvedaras.laravelquery.domain.query.builder.methods.parameters
 
-import com.intellij.openapi.project.DumbService
 import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.util.parentOfType
 import com.jetbrains.php.lang.psi.elements.Parameter
 import com.jetbrains.php.lang.psi.elements.Statement
+import dev.ekvedaras.laravelquery.support.whenSmart
 
 data class QueryParameter(val parameter: Parameter) {
-    fun usageStatements(): List<Statement> =
-        if (DumbService.isDumb(parameter.project))
-            listOf()
-        else ReferencesSearch.search(parameter.originalElement, parameter.resolveScope, false)
-            .toList()
-            .filterNot { it.element.originalElement == parameter.originalElement }
-            .mapNotNull { it.element.parentOfType() }
+    fun usageStatements(): List<Statement> = parameter.whenSmart {
+        ReferencesSearch.search(parameter.originalElement, parameter.resolveScope, false).toList().filterNot { it.element.originalElement == parameter.originalElement }.mapNotNull { it.element.parentOfType() }
+    } ?: listOf()
 }
